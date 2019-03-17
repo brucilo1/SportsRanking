e18a6c2c5865affa98f30e2372f7bac59e33b976
brucilo1 <brucilopez@hotmail.com>
2019-03-16 22:14:00 -0400

    Now we have a working program. The HomePage is now loading.  The buttons that lead to School Ranking and Leagues are now working. The breadcrumb section for School Ranking and Leagues pages are working.

--- src/main/java/edu/umuc/SportsRankingApp.java
+++ src/main/java/edu/umuc/SportsRankingApp.java
@@ -6,14 +6,34 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.File;
+import java.io.FileInputStream;
+
 public class SportsRankingApp extends Application {
+    //Static variables
+    private static final int DEFAULT_SCENE_WIDTH = 900;
+    private static final int DEFAULT_SCENE_HEIGHT = 700;
+    private static final String HOME_PAGE_URL = "src/main/java/edu/umuc/fxml/HomePage.fxml";
+
+    //Declaring Primary Stage here for re-usability in Controllers
+    private static Stage primaryStage;
+
+    private void setPrimaryStage(Stage stage) {
+        SportsRankingApp.primaryStage = stage;
+    }
+
+    static public Stage getPrimaryStage() {
+        return SportsRankingApp.primaryStage;
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-/*        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();*/
+        setPrimaryStage(primaryStage);
+
+        final FXMLLoader fxmlLoader = new FXMLLoader();
+        final Parent homePage = fxmlLoader.load(new FileInputStream(new File(HOME_PAGE_URL)));
+        primaryStage.setScene(new Scene(homePage, DEFAULT_SCENE_WIDTH, DEFAULT_SCENE_HEIGHT));
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
--- src/main/java/edu/umuc/controllers/Controller.java
+++ src/main/java/edu/umuc/controllers/Controller.java
@@ -1,4 +1,52 @@
 package edu.umuc.controllers;
 
+import edu.umuc.SportsRankingApp;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
 public class Controller {
+    private static final String SCHOOL_RANKING_URL = "src/main/java/edu/umuc/fxml/SchoolRanking.fxml";
+    private static final String LEAGUES_URL = "src/main/java/edu/umuc/fxml/Leagues.fxml";
+    private static final String HOME_PAGE_URL = "src/main/java/edu/umuc/fxml/HomePage.fxml";
+
+    @FXML
+    private Button btnSchoolsRanking;
+
+    @FXML
+    private Button btnLeagues;
+
+    @FXML
+    private Button btnHome;
+
+    @FXML
+    private void processButtonClickEvents(ActionEvent event) {
+        if (event.getSource() == btnSchoolsRanking) {
+            loadPage(SCHOOL_RANKING_URL);
+        } else if (event.getSource() == btnLeagues){
+            loadPage(LEAGUES_URL);
+        } else if (event.getSource() == btnHome){
+            loadPage(HOME_PAGE_URL);
+        }
+    }
+
+    private void loadPage(String fxmlUrl) {
+        try {
+            final FXMLLoader fxmlLoader = new FXMLLoader();
+            final Parent schoolRankingPage = fxmlLoader.load(new FileInputStream(new File(fxmlUrl)));
+            final Stage stage = SportsRankingApp.getPrimaryStage();
+            stage.setScene(new Scene(schoolRankingPage));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
